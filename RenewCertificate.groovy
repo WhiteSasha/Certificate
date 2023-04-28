5f9a2d227709f565b2ed1b194c250a21052a151a
whitesasha <white_sasha@mail.ru>
2023-04-28 11:25:39 +0300

    fix

--- RenewCertificate.groovy
+++ RenewCertificate.groovy
@@ -38,7 +38,8 @@ pipeline {
                     echo "Проверка подключения по SSH"
                     println "\033[34mПроверка подключения по SSH\033[0m"
                     //https://www.jenkins.io/doc/pipeline/steps/ssh-agent/
-                    sshagent(credentials: ['${devopsConfig.server.SSHCredentials}']) {
+
+                    sshagent(credentials: [${devopsConfig.server.SSHCredentials}]) {
                         sh 'ssh -o StrictHostKeyChecking=no white@192.168.40.180'
                         sh 'ssh white@192.168.40.180 whoami'
                         sh 'ssh white@192.168.40.180 ls'
