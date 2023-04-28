809dac5dbc3a90c5ab40f7bfc9830f68bfdadbec
Aleksandr Belov <white_sasha@mail.ru>
2023-04-29 01:33:30 +0300

    rm

--- RenewCertificate.groovy
+++ RenewCertificate.groovy
@@ -58,6 +58,7 @@ pipeline {
                     scp ./files/test.sh  root@192.168.40.109:/tmp/test.sh
                     ssh root@192.168.40.109 chmod +x /tmp/test.sh
                     ssh root@192.168.40.109 sh /tmp/test.sh
+                    ssh root@192.168.40.109 rm /tmp/test.sh
                     '''
 
 //                        sh 'ssh -o StrictHostKeyChecking=no white@192.168.40.180'
