1d00d2ec4ea2e2210a1ea57b4cdae78f6ef149e5
Aleksandr Belov <white_sasha@mail.ru>
2023-04-29 01:39:06 +0300

    echo

--- files/test.sh
+++ files/test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "==========### Just for test run shell script ###----------"
-cat /proc/sys/kernel/hostname
+HN=$(cat /proc/sys/kernel/hostname )
+echo "Hostname: $HN"
+
 pwd
 ls / -ls
 df
