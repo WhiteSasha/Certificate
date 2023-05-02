0e6a4c04e39ee807abf517640e8004b66cac2ca2
Aleksandr Belov <white_sasha@mail.ru>
2023-05-02 21:39:54 +0300

    RemoteHost

--- RenewCertificate.groovy
+++ RenewCertificate.groovy
@@ -45,6 +45,7 @@ pipeline {
 
         stage( 'Проверка подключения по SSH' ) {
             steps {
+                sh 'echo "RemoteHost: ${devopsConfig.server.RemoteHost}"'
                 script {
                     echo "Проверка подключения по SSH"
                     println "\033[34mПроверка подключения по SSH\033[0m"
@@ -55,7 +56,7 @@ pipeline {
                     sshagent(credentials: ["${devopsConfig.server.SSHCredentials}"]) {
 // Не могу передать переменную!!!
 //                    sh 'echo ${devopsConfig.server.RemoteHost}'
-                    sh 'echo "RemoteHost: ${devopsConfig.server.RemoteHost}"'
+//                    sh 'echo "RemoteHost: ${devopsConfig.server.RemoteHost}"'
                     sh '''
                     echo "---SSH---"
                     scp ./files/test.sh  root@192.168.40.109:/tmp/test.sh
