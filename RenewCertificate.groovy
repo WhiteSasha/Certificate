c6062e02fecc45225e051d6f9fdfe6f52a2fb18d
Aleksandr Belov <white_sasha@mail.ru>
2023-05-02 22:15:01 +0300

    d

--- RenewCertificate.groovy
+++ RenewCertificate.groovy
@@ -53,14 +53,14 @@ pipeline {
                     // Не могу передать переменную!!!
 
                     sh "echo '++==RemoteHost==++: ${devopsConfig.server.RemoteHost}'"
-                    sh "scp ./files/test.sh  ${devopsConfig.server.NewRemoteHost}:/tmp/test.sh"
-                    sh '''
+                    sh "scp ./files/test.sh  ${devopsConfig.server.SSHCredentials}:/tmp/test.sh"
+//                    sh '''
 //                    echo "---SSH---"
-                    scp ./files/test.sh  root@192.168.40.109:/tmp/test.sh
+//                    scp ./files/test.sh  root@192.168.40.109:/tmp/test.sh
 //                    ssh root@192.168.40.109 chmod +x /tmp/test.sh
 //                    ssh root@192.168.40.109 sh /tmp/test.sh
 //                    ssh root@192.168.40.109 rm /tmp/test.sh
-                    '''
+//                    '''
 
 //                        sh 'ssh -o StrictHostKeyChecking=no white@192.168.40.180'
 //                        sh 'echo "---SSH---"'
