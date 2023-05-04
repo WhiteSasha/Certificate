e3152f48a37cec203996cf819548cd0f9bdedbfb
Aleksandr Belov <white_sasha@mail.ru>
2023-05-04 21:24:02 +0300

    seafile.alex-white.ru

--- RenewCertificate.groovy
+++ RenewCertificate.groovy
@@ -16,7 +16,7 @@ pipeline {
 //Параметризированая джоба. https://devopscube.com/declarative-pipeline-parameters/ Параметры в UI джобы появятся после первого запуска
     parameters{
         choice(
-            choices: ['nexus.alex-white.ru', 'jenkins.alex-white.ru', 'seafiles.alex-white.ru', 'alex-white.ru'], 
+            choices: ['nexus.alex-white.ru', 'seafile.alex-white.ru', 'jenkins.alex-white.ru'], 
             name: 'domain'
         )
         booleanParam(
